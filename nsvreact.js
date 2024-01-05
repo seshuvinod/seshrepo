de9a0eb46f79186a1250ae4c2a0585dd766e7138
vinodns141 <vinodns141@gmail.com>
2024-01-05 13:28:43 -0500

    secondcommit

--- nsvreact.js
+++ nsvreact.js
@@ -3,11 +3,29 @@ import ReactDOM  from 'react-dom/client'
 // import  ReactDOM from 'react-dom/client'
 
 
+
+//Javascript to display Helloworld
+
 // let header=document.createElement('h1');
 // header.textContent='Hello from JS'
 // let rootElement=document.getElementById('root')
 // rootElement.append(header);
-let header=React.createElement('div',{id:'rootid'},[React.createElement('h1',{id:'h1id'},"Hello World"),React.createElement('h2',{id:'h2id'},"Hello World"),React.createElement('h3',{id:'h3id'},"Hello World")])
+
+
+//React to Dislay Helloworld without jsx
+
+// let header=React.createElement('div',{id:'rootid'},[React.createElement('h1',{id:'h1id'},"Hello World"),React.createElement('h2',{id:'h2id'},"Hello World"),React.createElement('h3',{id:'h3id'},"Hello World")])
+ //let root=ReactDOM.createRoot(document.getElementById('root'))
+ //root.render(<reactval/>)
+
+let Reactval=()=>{
+return <div className='class1'>
+      <h1>Hello from JSX</h1>
+      <h1>Hello from JSX 2</h1>
+</div>
+}
+
+
 let root=ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(header)
+root.render(<Reactval/>)
